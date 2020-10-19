c280b7d8bff337cacf02354240fc469c6d821f10
unknown <you@example.com>
2020-10-19 13:49:27 +0300

    Add new function

---
+++ convert.py
@@ -0,0 +1,13 @@
+import os, glob
+from pydub import AudioSegment as am
+dir_ = "C:/Users/Lyudmila/Desktop/emotion/test/sad/"
+os.chdir(dir_)
+count = 0
+for file_ in glob.glob("*.wav"):
+    file_tmp = am.from_file(file_, format='wav', frame_rate=48000)
+#sound = am.from_file("D:/tmp/digits_rus/ноль/010 ноль.wav", format='wav', frame_rate=8000)
+#sound = sound.set_frame_rate(16000)
+    file_tmp = file_tmp.set_frame_rate(16000)
+    file_tmp.export('C:/Users/Lyudmila/Desktop/emotion/test_16000/sad/' + "m_" + file_, format='wav')
+    count += 1
+#sound.export('D:/tmp/digits_rus/ноль/010_ноль.wav', format='wav')
